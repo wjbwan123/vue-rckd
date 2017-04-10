a7466081b3186bf113d17120295e15ad0284227a
vue-rckd <565835115@qq.com>
2017-04-10 10:02:28 +0800

    hy

--- src/main/java/net/rckd/utils/Md5Utils.java
+++ src/main/java/net/rckd/utils/Md5Utils.java
@@ -10,7 +10,7 @@ public class Md5Utils {
 
     public static String getMd5String(String password){
         //盐
-        final String salt = "dfdgdfgfd";
+        final String salt = "565835115@qq.com";
         //散列次数
         final int hashIterations = 1;
         Md5Hash md5 = new Md5Hash(password, salt, hashIterations);
